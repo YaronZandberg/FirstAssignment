19ce2dd4695d332c2286614870805cd9180a8a60
YaronZandberg <52877527+YaronZandberg@users.noreply.github.com>
2022-12-03 17:34:04 +0200

    Fixed the problem of the win strike image

--- app/src/main/java/com/example/firstassignment/CellPosition.java
+++ app/src/main/java/com/example/firstassignment/CellPosition.java
@@ -1,17 +1,7 @@
 package com.example.firstassignment;
 
 public enum CellPosition {
-    EMPTY("empty"),
-    RED("X"),
-    BLUE("O");
-
-    private String cellSign;
-
-    CellPosition(String cellSign){
-        this.cellSign = cellSign;
-    }
-
-    public String getLabel(){
-        return this.cellSign;
-    }
+    EMPTY,
+    RED,
+    BLUE
 }
--- app/src/main/java/com/example/firstassignment/MainActivity.java
+++ app/src/main/java/com/example/firstassignment/MainActivity.java
@@ -9,66 +9,56 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private final Integer MAX_MOVES = 9;
     private final Integer O_PLAYER = 0;
     private final Integer X_PLAYER = 1;
-    private Integer movesCounter;
-    private boolean isGameActive = false;
-    private Integer activePlayer = X_PLAYER;
-    private CellPosition[] gameState = new CellPosition[this.MAX_MOVES];
+    private final List<ImageView> boardCells = new ArrayList();
+    private final CellPosition[] gameState = new CellPosition[this.MAX_MOVES];
+    private final Integer[] cellIDs = {R.id.board_cell1, R.id.board_cell2, R.id.board_cell3,
+            R.id.board_cell4, R.id.board_cell5, R.id.board_cell6, R.id.board_cell7,
+            R.id.board_cell8, R.id.board_cell9};
+    private final Integer[] winsSituations = {R.drawable.high, R.drawable.horizontal, R.drawable.low,
+            R.drawable.left, R.drawable.vertical, R.drawable.right,
+            R.drawable.slashwin, R.drawable.backslashwin};
     private final Integer[][] winPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
             {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
             {0, 4, 8}, {2, 4, 6}};
+    private boolean isGameActive = false;
+    private Integer movesCounter;
+    private Integer activePlayer;
     private Button startGameBtn;
     private ImageView playerMessage;
     private ImageView winStrike;
-    //private List<ImageView> boardCells = new ArrayList();
-    private ImageView cell1;
-    private ImageView cell2;
-    private ImageView cell3;
-    private ImageView cell4;
-    private ImageView cell5;
-    private ImageView cell6;
-    private ImageView cell7;
-    private ImageView cell8;
-    private ImageView cell9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.activePlayer = X_PLAYER;
         this.playerMessage = findViewById(R.id.board_player_message);
         this.playerMessage.setVisibility(View.INVISIBLE);
         this.winStrike = findViewById(R.id.board_win_strike);
         this.winStrike.setVisibility(View.INVISIBLE);
         this.startGameBtn = findViewById(R.id.board_play_again_button);
         this.startGameBtn.setText("Start Game");
-        this.cell1 = findViewById(R.id.board_cell1);
-        this.cell2 = findViewById(R.id.board_cell2);
-        this.cell3 = findViewById(R.id.board_cell3);
-        this.cell4 = findViewById(R.id.board_cell4);
-        this.cell5 = findViewById(R.id.board_cell5);
-        this.cell6 = findViewById(R.id.board_cell6);
-        this.cell7 = findViewById(R.id.board_cell7);
-        this.cell8 = findViewById(R.id.board_cell8);
-        this.cell9 = findViewById(R.id.board_cell9);
+        initializeCells(this.boardCells);
         this.startGameBtn.setOnClickListener((view) -> startGame());
-        this.cell1.setOnClickListener(this::playerTap);
-        this.cell2.setOnClickListener(this::playerTap);
-        this.cell3.setOnClickListener(this::playerTap);
-        this.cell4.setOnClickListener(this::playerTap);
-        this.cell5.setOnClickListener(this::playerTap);
-        this.cell6.setOnClickListener(this::playerTap);
-        this.cell7.setOnClickListener(this::playerTap);
-        this.cell8.setOnClickListener(this::playerTap);
-        this.cell9.setOnClickListener(this::playerTap);
     }
 
-    public void startGame() {
+    private void initializeCells(List<ImageView> boardCells) {
+        for (int i = 0; i < this.cellIDs.length; i++) {
+            boardCells.add(findViewById(this.cellIDs[i]));
+            boardCells.get(i).setOnClickListener(this::playerTurn);
+        }
+    }
+
+    private void startGame() {
         resetGame();
         this.isGameActive = true;
         this.startGameBtn.setVisibility(View.INVISIBLE);
@@ -77,37 +67,23 @@ public class MainActivity extends AppCompatActivity {
         this.playerMessage.setVisibility(View.VISIBLE);
     }
 
-    public void resetGame() {
+    private void resetGame() {
         this.activePlayer = X_PLAYER;
         this.movesCounter = 0;
         Arrays.fill(this.gameState, EMPTY);
-        this.cell1.setImageResource(R.drawable.empty);
-        this.cell1.setTag(1);
-        this.cell2.setImageResource(R.drawable.empty);
-        this.cell2.setTag(2);
-        this.cell3.setImageResource(R.drawable.empty);
-        this.cell3.setTag(3);
-        this.cell4.setImageResource(R.drawable.empty);
-        this.cell4.setTag(4);
-        this.cell5.setImageResource(R.drawable.empty);
-        this.cell5.setTag(5);
-        this.cell6.setImageResource(R.drawable.empty);
-        this.cell6.setTag(6);
-        this.cell7.setImageResource(R.drawable.empty);
-        this.cell7.setTag(7);
-        this.cell8.setImageResource(R.drawable.empty);
-        this.cell8.setTag(8);
-        this.cell9.setImageResource(R.drawable.empty);
-        this.cell9.setTag(9);
+        for (int i = 0; i < this.MAX_MOVES; i++) {
+            this.boardCells.get(i).setImageResource(R.drawable.empty);
+            this.boardCells.get(i).setTag(i);
+        }
     }
 
-    public void endGame() {
+    private void endGame() {
         this.isGameActive = false;
         this.startGameBtn.setText("Play Again");
         this.startGameBtn.setVisibility(View.VISIBLE);
     }
 
-    public void playerTap(View view) {
+    private void playerTurn(View view) {
         if (this.isGameActive) {
             ImageView currentBoardCellImage = (ImageView) view;
             Integer tappedCellNumber = Integer.parseInt(currentBoardCellImage.getTag().toString());
@@ -137,43 +113,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void checkWinSituation() {
+    private void checkWinSituation() {
         for (int i = 0; i < this.winPositions.length; i++) {
-            if (this.gameState[this.winPositions[i][0]] == this.gameState[this.winPositions[i][1]] &&
-                    this.gameState[this.winPositions[i][1]] == this.gameState[this.winPositions[i][2]] &&
-                    this.gameState[this.winPositions[i][0]] != EMPTY) {
-                switch (i) {
-                    case 0:
-                        this.winStrike.setImageResource(R.drawable.high);
-                        break;
-                    case 1:
-                        this.winStrike.setImageResource(R.drawable.horizontal);
-                        break;
-                    case 2:
-                        this.winStrike.setImageResource(R.drawable.low);
-                        break;
-                    case 3:
-                        this.winStrike.setImageResource(R.drawable.left);
-                        break;
-                    case 4:
-                        this.winStrike.setImageResource(R.drawable.vertical);
-                        break;
-                    case 5:
-                        this.winStrike.setImageResource(R.drawable.right);
-                        break;
-                    case 6:
-                        this.winStrike.setImageResource(R.drawable.slashwin);
-                        break;
-                    case 7:
-                        this.winStrike.setImageResource(R.drawable.backslashwin);
-                        break;
-                    default:
-                        break;
-                }
+            if (!Objects.equals(this.gameState[this.winPositions[i][0]], EMPTY) &&
+                    Objects.equals(this.gameState[this.winPositions[i][0]], this.gameState[this.winPositions[i][1]]) &&
+                    Objects.equals(this.gameState[this.winPositions[i][1]], this.gameState[this.winPositions[i][2]])) {
 
+                this.winStrike.setImageResource(this.winsSituations[i]);
                 this.winStrike.setVisibility(View.VISIBLE);
 
-                if (this.gameState[this.winPositions[i][0]] == BLUE) {
+                if (Objects.equals(this.gameState[this.winPositions[i][0]], BLUE)) {
                     this.playerMessage.setImageResource(R.drawable.owin);
                 } else {
                     this.playerMessage.setImageResource(R.drawable.xwin);
